5e821a958c06b4005dd5a398845c0104aad45e36
Tom Pauwels <tom.pauwels@outlook.be>
2016-04-22 19:38:37 +0200

    Forgot to add files

---
+++ src/MakeIT.Nop.Plugins.Shipment.BPost/OrderPlacedEventHandler.cs
@@ -0,0 +1,30 @@
+﻿using Nop.Core;
+using Nop.Core.Domain.Orders;
+using Nop.Services.Common;
+using Nop.Services.Events;
+
+namespace MakeIT.Nop.Plugin.Shipping.Bpost.ShippingManager
+{
+    public class OrderPlacedEventHandler : IConsumer<OrderPlacedEvent>
+    {
+        private readonly IWorkContext _workContext;
+        private readonly IStoreContext _storeContext;
+        private readonly IGenericAttributeService _genericAttributeService;
+
+        public OrderPlacedEventHandler(
+            IWorkContext workContext, IStoreContext storeContext,
+            IGenericAttributeService genericAttributeService)
+        {
+            _workContext = workContext;
+            _storeContext = storeContext;
+            _genericAttributeService = genericAttributeService;
+        }
+
+        public void HandleEvent(OrderPlacedEvent eventMessage)
+        {
+            //_genericAttributeService.SaveAttribute<decimal>(_workContext.CurrentCustomer, CustomCustomerAttributeNames.DeliveryMethodRate, 0, _storeContext.CurrentStore.Id);
+            //_genericAttributeService.SaveAttribute(_workContext.CurrentCustomer, CustomCustomerAttributeNames.DeliveryMethodAddress, "", _storeContext.CurrentStore.Id);
+            //_genericAttributeService.SaveAttribute(_workContext.CurrentCustomer, CustomCustomerAttributeNames.DeliveryMethod, "", _storeContext.CurrentStore.Id);
+        }
+    }
+}
---
+++ src/MakeIT.Nop.Plugins.Shipment.BPost/WorkContextExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nop.Core;
+using Nop.Core.Domain.Customers;
+using Nop.Core.Domain.Shipping;
+using Nop.Services.Common;
+
+namespace MakeIT.Nop.Plugin.Shipping.Bpost.ShippingManager
+{
+    public static class WorkContextExtension
+    {
+        public static ShippingOption GetBpostShippingOption(this IWorkContext workContext, IStoreContext storeContext)
+        {
+            var shippingOptions =
+                workContext.CurrentCustomer.GetAttribute<List<ShippingOption>>(
+                    SystemCustomerAttributeNames.OfferedShippingOptions, storeContext.CurrentStore.Id);
+
+            return
+                shippingOptions?.FirstOrDefault(
+                    so => so.ShippingRateComputationMethodSystemName.Equals("Shipping.Bpost.ShippingManager", StringComparison.InvariantCultureIgnoreCase));
+        }
+    }
+}
